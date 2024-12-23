7be1a2d81cc3b57ee3651250b6991fed152c9218
ivandrodri <ivandiegorodriguez@gmail.com>
2024-12-23 12:28:37 +0100

    minor changes

---
+++ src/generate_custom_minari_datasets/download_minari_datasets.py
@@ -0,0 +1,28 @@
+import minari
+
+from generate_custom_minari_datasets.generate_minari_dataset_grid_envs import MinariDatasetConfig
+
+
+def download_minari_dataset(data_set_name: str):
+    """
+    Download a Minari dataset from the cloud and save some meta info locally
+    :param data_set_name: one of minari datasets, for instance "relocate-cloned-v1"
+    :return:
+    """
+
+    minari.download_dataset(data_set_name)
+
+    data = minari.load_dataset(data_set_name)
+
+    minari_config = MinariDatasetConfig(
+        env_name="See: https://minari.farama.org/main/",
+        data_set_name=data_set_name,
+        num_steps=data.total_steps,
+    )
+    minari_config.save_to_file()
+
+
+#if __name__ == "__main__":
+#    DATASET_NAME ="pen-human-v2"
+
+#    download_minari_dataset(DATASET_NAME)
---
+++ src/offline_policies/bcq_continuous_policy.py
@@ -0,0 +1,113 @@
+from typing import Any, Dict
+import gymnasium as gym
+import torch
+from tianshou.policy import BCQPolicy, BasePolicy
+from tianshou.utils.net.common import MLP, Net
+from tianshou.utils.net.continuous import VAE, Critic, Perturbation
+from utils import extract_dimension
+
+policy_config = {
+    "hidden_sizes": [256, 256],
+    "device": "cpu",
+    "phi": 0.05,  # 0.05
+    "actor_lr": 1e-3,
+    "critic_lr": 1e-3,
+    "vae_hidden_sizes": [512, 512],
+    "gamma": 0.99,
+    "tau": 0.005,
+    "lmbda": 0.75,
+    "forward_sampled_times": 100,
+    "num_sampled_action": 10,
+}
+
+
+def bcq_continuous_default_config() -> Dict[str, Any]:
+    return policy_config
+
+
+def create_bcq_continuous_policy_from_dict(
+    policy_config: Dict[str, Any],
+    action_space: gym.core.ActType,
+    observation_space: gym.core.ObsType,
+) -> BasePolicy:
+    observation_shape = extract_dimension(observation_space)
+    action_shape = extract_dimension(action_space)
+
+    # model
+    # perturbation network
+    net_a = MLP(
+        input_dim=observation_shape + action_shape,
+        output_dim=action_shape,
+        hidden_sizes=policy_config["hidden_sizes"],
+        device=policy_config["device"],
+    )
+
+    max_action = action_space.high[0]
+    actor = Perturbation(
+        net_a, max_action=max_action, device=policy_config["device"], phi=policy_config["phi"]
+    ).to(policy_config["device"])
+    actor_optim = torch.optim.Adam(actor.parameters(), lr=policy_config["actor_lr"])
+
+    net_c1 = Net(
+        observation_shape,
+        action_shape,
+        hidden_sizes=policy_config["hidden_sizes"],
+        concat=True,
+        device=policy_config["device"],
+    )
+    net_c2 = Net(
+        observation_shape,
+        action_shape,
+        hidden_sizes=policy_config["hidden_sizes"],
+        concat=True,
+        device=policy_config["device"],
+    )
+    critic1 = Critic(net_c1, device=policy_config["device"]).to(policy_config["device"])
+    critic1_optim = torch.optim.Adam(critic1.parameters(), lr=policy_config["critic_lr"])
+    critic2 = Critic(net_c2, device=policy_config["device"]).to(policy_config["device"])
+    critic2_optim = torch.optim.Adam(critic2.parameters(), lr=policy_config["critic_lr"])
+
+    # vae
+    # output_dim = 0, so the last Module in the encoder is ReLU
+    vae_encoder = MLP(
+        input_dim=observation_shape + action_shape,
+        hidden_sizes=policy_config["vae_hidden_sizes"],
+        device=policy_config["device"],
+    )
+
+    latent_dim = action_shape * 2
+    vae_decoder = MLP(
+        input_dim=observation_shape + latent_dim,
+        output_dim=action_shape,
+        hidden_sizes=policy_config["vae_hidden_sizes"],
+        device=policy_config["device"],
+    )
+    vae = VAE(
+        vae_encoder,
+        vae_decoder,
+        hidden_dim=policy_config["vae_hidden_sizes"][-1],
+        latent_dim=latent_dim,
+        max_action=max_action,
+        device=policy_config["device"],
+    ).to(policy_config["device"])
+    vae_optim = torch.optim.Adam(vae.parameters())
+
+    policy = BCQPolicy(
+        actor_perturbation=actor,
+        actor_perturbation_optim=actor_optim,
+        critic=critic1,
+        critic_optim=critic1_optim,
+        critic2=critic2,
+        critic2_optim=critic2_optim,
+        vae=vae,
+        vae_optim=vae_optim,
+        device=policy_config["device"],
+        gamma=policy_config["gamma"],
+        tau=policy_config["tau"],
+        lmbda=policy_config["lmbda"],
+        forward_sampled_times=policy_config["forward_sampled_times"],
+        num_sampled_action=policy_config["num_sampled_action"],
+        action_space=action_space,
+    )
+
+    return policy
