95c5becd46a99018d8f954d1d31a2b721d25649a
ivandrodri <ivandiegorodriguez@gmail.com>
2025-01-04 12:52:07 +0100

    few tests added.

--- tests/test_minari.py
+++ tests/test_minari.py
@@ -8,6 +8,8 @@ from offline_rl.generate_custom_minari_datasets.generate_minari_dataset import (
     create_minari_datasets,
 )
 
+DATA_SET_IDENTIFIER = "_collected_data_test"
+
 
 class TestMinari:
     @pytest.mark.parametrize(
@@ -25,7 +27,7 @@ class TestMinari:
         set_test_env_variables,
     ):
         env = get_grid_8x8_env
-        test_data_identifier = "_collected_data_test"
+        test_data_identifier = DATA_SET_IDENTIFIER
         num_traj_steps = 3
 
         grid_env_config = Grid2DInitialConfig(
@@ -44,7 +46,13 @@ class TestMinari:
         assert data_set_config.num_steps == num_traj_steps
         assert data_set_config.behavior_policy == random_grid_policy
 
-    def test_load_minari(self):
+    def test_create_tianshou_reply_buffer_from_minari(self):
+        # buffer_data = load_buffer_minari(data_set_config.data_set_name)
+        # print(f"The number of dataset points is {len(buffer_data)}")
+
+        # for elem in buffer_data:
+        #    print(elem)
+        #    break
         pass
 
     def test_combine_minari(self):
